f4337e9f8a301a7684e9c49658acfe47ee72dd2b
SSR <someshwar.sangaraju19@gmail.com>
2025-10-28 16:11:40 +0530

    terraform

--- vpc.tf
+++ vpc.tf
@@ -116,3 +116,10 @@ resource "aws_route_table" "database" {
   )
 }
 
+# public route
+resource "aws_route" "public" {
+  route_table_id            = aws_route_table.public.id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.internet_gateway.id
+}
+
